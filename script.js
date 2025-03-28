80d8ee68e9cc0d6820bd9828b1a22c4db8a4ea0f
mawpay <rahmah300305@gmail.com>
2025-03-28 20:03:14 +0800

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById("belanja-btn").addEventListener("click", function() {
-    alert("Silakan pilih produk Anda dan lanjutkan transaksi melalui WhatsApp atau Instagram kami!");
+    alert("Silakan pilih produk Anda dan lanjutkan transaksi melalui WhatsApp atau Instagram kami yang tertera di halaman Kontak!");
 });
